6f7535dc141487b2c8475d47a0f4a684caca3407
tienminhvy <tienminhvydev@gmail.com>
2023-11-10 10:34:41 +0700

    Cập nhật DTO một số class

--- DTO/NhacCu.cs
+++ DTO/NhacCu.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace QLBanPiano.DTO
 {
-    public class NhacCu
+    public class NhacCu : DoiTuong
     {
+        private int id;
+        private string ma;
+        private string ten;
+        private string dacDiemNoiBat;
+        private string moTaChiTiet;
+        private int gia;
+        private string hinhAnh;
+
+        public int Id { get { return id; } set {  id = value; } }
+        public string Ma { get { return ma; } set { ma = value; } }
+        public string Ten { get { return ten; } set { ten = value; } }
+        public string DacDiemNoiBat { get {  return dacDiemNoiBat; } set {  dacDiemNoiBat = value; } }
+        public string MoTaChiTiet { get { return moTaChiTiet; } set { moTaChiTiet = value; } }
+        public int Gia { get { return gia; } set {  gia = value; } }
+        public string HinhAnh { get {  return hinhAnh; } set {  hinhAnh = value; } }
+
+        public NhacCu()
+        {
+            this.id = -1;
+            this.ma = "";
+            this.ten = "";
+            this.dacDiemNoiBat = "";
+            this.moTaChiTiet = "";
+            this.gia = 0;
+            this.hinhAnh = "";
+        }
+        public NhacCu(int id, string ma, string ten, string dacDiemNoiBat,
+            string moTaChiTiet, int gia, string hinhAnh)
+        {
+            this.id = id;
+            this.ma = ma;
+            this.ten = ten;
+            this.dacDiemNoiBat = dacDiemNoiBat;
+            this.moTaChiTiet = moTaChiTiet;
+            this.gia = gia;
+            this.hinhAnh = hinhAnh;
+        }
     }
 }
--- DTO/Piano.cs
+++ DTO/Piano.cs
@@ -8,5 +8,25 @@ namespace QLBanPiano.DTO
 {
     public class Piano
     {
+        private int id;
+        private string phanloai;
+        private NhacCu nhacCu;
+
+        public int Id { get => id; set => id = value; }
+        public string Phanloai { get => phanloai; set => phanloai = value; }
+        public NhacCu NhacCu { get => nhacCu; set => nhacCu = value; }
+
+        public Piano() {
+            id = -1;
+            phanloai = "";
+            nhacCu = null;
+        }
+
+        public Piano(int id, string phanloai, NhacCu nhacCu)
+        {
+            this.id = id;
+            this.phanloai = phanloai;
+            this.nhacCu = nhacCu;
+        }
     }
 }
